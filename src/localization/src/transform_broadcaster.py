5418f50a545189107bf1d9f670242d26407df254
agus-darmawan <wayanagus.dr@gmail.com>
2024-10-20 14:07:23 +0700

    feat: add localization based on odom and imu

---
+++ src/localization/src/transform_broadcaster.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+class TransformBroadcaster:
+    def __init__(self):
+        rospy.init_node('transform_broadcaster_node')
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.odom_subscriber = rospy.Subscriber('/leg_odom', PoseWithCovarianceStamped, self.odom_callback)
+        self.imu_subscriber = rospy.Subscriber('/imu/data', Imu, self.imu_callback)
+
+        self.position = None
+        self.orientation = None
+
+    def odom_callback(self, msg):
+        self.position = msg.pose.pose.position
+
+    def imu_callback(self, msg):
+        self.orientation = msg.orientation
+
+    def publish_transform(self):
+        transform = geometry_msgs.msg.TransformStamped()
+        transform.header.stamp = rospy.Time.now()
+        transform.header.frame_id = "map"
+        transform.child_frame_id = "nyk/base_link"
+
+        if self.position is not None and self.orientation is not None:
+            transform.transform.translation.x = self.position.x
+            transform.transform.translation.y = self.position.y
+            transform.transform.translation.z = self.position.z
+            transform.transform.rotation = self.orientation
+
+            self.broadcaster.sendTransform(transform)
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.publish_transform()
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        transform_broadcaster = TransformBroadcaster()
+        transform_broadcaster.run()
+    except rospy.ROSInterruptException:
+        pass
+
